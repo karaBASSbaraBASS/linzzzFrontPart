e4a6a80d6521575d3b404a7e56aede0590f3a4c8
Jack <evgen.mikhalov@gmail.com>
2019-01-04 05:18:39 +0200

    first page insights

--- public/services.php
+++ public/services.php
@@ -6,15 +6,66 @@
             <div class="flexCol">
                 <div class="headingWrap">
                     <div class="headingBlock">
-                        <h1>LIQUID ASSETS</h1>
+                        <h1>services</h1>
                     </div>
                 </div>
-                <span class="mainSubheading text-center col-70">Your wealth management. Crafted for performance</span>
-                <div class="col-80 text-block">
-                    <p>When it comes to the custody of your bankable assets, you aim for nothing less than excellence. And this is exactly what we provide at Linvo: clear and detailed overview of your assets, with in – depth analysis and thorough risk management, every step of the way.</p>
-                    <p>Using our substantial knowledge and experience in the equity and forex markets, we can help you navigate the financial markets to make investments in line with your personal objectives.</p>
-                    <p>Our advisory & discretionary mandates offer you exceptional, bespoke financial guidance, while keeping you in full command of your investment decisions (advisory mandates) or in complete comfort on our handling your investment performance (discretionary mandates).</p>
+                <div class="servicesSquaresWrap">
+                    <div class="servicesSquares__item">
+                        <div class="imageWrap">
+                            <img src="/img/bg/box_illiquid_bw.png" alt="squareBg">
+                        </div>
+                        <div class="redShadow"></div>
+                        <div class="floatingHeader">
+                            <h4>LIQUID ASSETS</h4>
+                        </div>
+                        <div class="floatingCaption">
+                            <span>Your wealth management. Crafted for performance.</span>
+                        </div>
+                    </div>
+                    <div class="servicesSquares__item">
+                        <div class="imageWrap">
+                            <img src="/img/bg/box_illiquid_bw.png" alt="squareBg">
+                        </div>
+                        <div class="redShadow"></div>
+                        <div class="floatingHeader">
+                            <h4>Illiquid Assets</h4>
+                        </div>
+                        <div class="floatingCaption">
+                            <span>Your opportunities. Expertly enhanced.</span>
+                        </div>
+                    </div>
+                    <div class="servicesSquares__item">
+                        <div class="imageWrap">
+                            <img src="/img/bg/box_family_bw.png" alt="squareBg">
+                        </div>
+                        <div class="redShadow"></div>
+                        <div class="floatingHeader">
+                            <h4>Family Office</h4>
+                        </div>
+                        <div class="floatingCaption">
+                            <span>Your most cherished values. Perfectly orchestrated.</span>
+                        </div>
+                    </div>
+                    <div class="servicesSquares__item">
+                        <div class="imageWrap">
+                            <img src="/img/bg/box_alternative_bw.png" alt="squareBg">
+                        </div>
+                        <div class="redShadow"></div>
+                        <div class="floatingHeader">
+                            <h4>Alternative Investments</h4>
+                        </div>
+                        <div class="floatingCaption">
+                            <span>Your unique interests. Successfully connected.</span>
+                        </div>
+                    </div>
                 </div>
+
+<!--                <span class="mainSubheading text-center col-70">Your wealth management. Crafted for performance</span>-->
+<!--                <div class="col-80 text-block">-->
+<!--                    <p>When it comes to the custody of your bankable assets, you aim for nothing less than excellence. And this is exactly what we provide at Linvo: clear and detailed overview of your assets, with in – depth analysis and thorough risk management, every step of the way.</p>-->
+<!--                    <p>Using our substantial knowledge and experience in the equity and forex markets, we can help you navigate the financial markets to make investments in line with your personal objectives.</p>-->
+<!--                    <p>Our advisory & discretionary mandates offer you exceptional, bespoke financial guidance, while keeping you in full command of your investment decisions (advisory mandates) or in complete comfort on our handling your investment performance (discretionary mandates).</p>-->
+<!--                </div>-->
             </div>
         </div>
     </section>
